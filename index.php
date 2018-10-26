7302702cd6498317b6ef2a312471d68a63166045
Krokozebra <vobliy@mail.ru>
2018-10-26 11:46:24 +1000

    первай коммит

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<form method="get" action="index.php">
+<input type="text" value="" name="name">
+<input type="submit" name="but" value="Жми">
+</form>
+<?php 
+if ( isset($_GET['but']) )
+{
+	$data=$_GET;
+
+$pic='ПРиВет '.$data['name'].'<br><img src="bg.jpg" alt="">';
+echo $pic;
+
+
+}
+?>
